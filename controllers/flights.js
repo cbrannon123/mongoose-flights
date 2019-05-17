fb06feee067e9d0b9e03ab80aee8a987ef9f691b
Casey Brannon <cbrannon412@gmail.com>
2019-05-17 14:24:53 -0700

    rendering new

---
+++ controllers/flights.js
@@ -0,0 +1,12 @@
+var Flight = require('../models/flight')
+
+module.exports = {
+    new: newFlight,
+    
+};
+
+
+
+function newFlight(req, res) {
+    res.render('flights/new')
+}    
---
+++ models/flight.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+//short cut
+var Schema = mongoose.Schema;
+
+var flightSchema = new Schema({
+    airline: String,
+    flightNo: Number,
+    departs: Date
+});
+
+//complile the schema into a modle and export
+module.exports = mongoose.model('Flight', flightSchema);
+
+
+
+
+
+
+
+
+
+
+

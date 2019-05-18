e667dc3b111db45703cacc01984396170a16b7c8
Casey Brannon <cbrannon412@gmail.com>
2019-05-17 17:04:01 -0700

    rendering flights

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,11 +1,19 @@
+//controllers
+
+//retrieves flight model
 var Flight = require('../models/flight')
 
 module.exports = {
     new: newFlight,
-    
+    create
 };
 
-
+function create(req, res) {
+    Flight = Flight(req.body)
+    console.log(Flight);
+    res.render('flights/new')
+    
+}
 
 function newFlight(req, res) {
     res.render('flights/new')
--- routes/flights.js
+++ routes/flights.js
@@ -1,8 +1,12 @@
+//routes
+
 var express = require('express');
 var router = express.Router();
 var flightsCtrl = require('../controllers/flights');
 
 /* GET /movies/new */
-
 router.get('/new', flightsCtrl.new);
+router.post('/', flightsCtrl.create);
+
+
 module.exports = router;
